5a8bbf21c6fd5fe13834b856f8aa93093839d6e4
Elias Nau <contact@eliasnau.dev>
2026-01-02 16:38:29 +0100

    feat: make search redirect client side

---
+++ src/app/search/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { useEffect, Suspense } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import bangs from "@/lib/bangs";
+
+function SearchContent() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const q = searchParams.get("q");
+
+  useEffect(() => {
+    if (!q) return;
+    let searchUrl;
+
+    const query = q;
+    const bangMatch = query.match(/!(\S+)/i);
+    const bangCandidate = bangMatch?.[1]?.toLowerCase();
+
+    const shortcutMatches = query.match(/#(\S+)/g) || [];
+    const shortcuts = shortcutMatches.map((match) => match.slice(1).toLowerCase());
+
+    const defaultEngine = localStorage.getItem("defaultEngine") || "g";
+
+    const defaultBang =
+      bangs.find((b) => b.bang === defaultEngine) ||
+      bangs.find((b) => b.bang === "g") ||
+      bangs[0];
+    const selectedBang = bangCandidate
+      ? bangs.find((b) => b.bang === bangCandidate) || defaultBang
+      : defaultBang;
+
+    let cleanQuery = query.replace(/!\S+\s*/i, "").trim();
+
+    shortcuts.forEach((shortcut) => {
+      const bangMatch = bangs.find((b) => b.bang === shortcut);
+      if (bangMatch && bangMatch.s) {
+        const shortcutRegex = new RegExp(`#${shortcut}\\b`, "g");
+        while (shortcutRegex.test(cleanQuery)) {
+          cleanQuery = cleanQuery
+            .replace(shortcutRegex, bangMatch.s)
+            .replace(/\s+/g, " ")
+            .trim();
+        }
+      }
+    });
+
+    if (cleanQuery === "") {
+      searchUrl = "https://" + selectedBang.siteUrl;
+    } else {
+      searchUrl = selectedBang.searchTemplate.replace(
+        "%s",
+        encodeURIComponent(cleanQuery).replace(/%2F/g, "/")
+      );
+    }
+
+    if (searchUrl) {
+      router.replace(searchUrl);
+    }
+  }, [q, router]);
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-background dark:bg-gray-900"></div>
+  );
+}
+
+export default function Search() {
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center min-h-screen bg-background dark:bg-gray-900"></div>
+      }
+    >
+      <SearchContent />
+    </Suspense>
+  );
+}
